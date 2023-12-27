0d2b8ac1b28ceee53ab883fe9d026d45f8686a63
koli-pravin <koli.pravin3@gmail.com>
2023-12-27 11:10:36 +0530

    add new course_2

---
+++ Course_2.py
@@ -0,0 +1,5 @@
+print(name : PRAVIN KOLI)
+
+print(The module name of from Course 2 : Module 6 Unsupervised Learning: Clustering)
+
+print(3-4 sentences on your learnings from that module : Clustering is a best and intersting part of data science enggineering part. this is the unsupervised classification problem solving method. this method or data is not having label or any target. we need to create the clusters by understanding a data and make a model and evaluate it.)
--- Data_Processing.py
+++ Data_Processing.py
@@ -8,4 +8,4 @@ def sum(a,b):
     c = a+b
     return a
 
-print('added a new function sum which is not correct')
+print('added a new function sum which is not correct') 
